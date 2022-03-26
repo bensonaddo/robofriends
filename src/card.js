b2905c32881500b6097b2641e428375cbe8e2391
baddo1 <benson.addo@gmfinancial.com>
2022-03-25 20:01:42 -0500

    removed dead scripts and added comments

--- src/card.js
+++ src/card.js
@@ -3,14 +3,10 @@ import 'tachyons';
 
 class Card extends Component {
     render(){
+        // Define variables to be used for card properties
         const { urName, image, email } = this.props
         return(
             <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                {/* <img src={this.props.image} alt="robots" />
-                <div className="tc">
-                    <h2>{this.props.urName}</h2>
-                    <p>{this.props.email}</p>
-                </div> */}
                 <img src={image} alt="robots" />
                 <div className="tc">
                     <h2>{urName}</h2>
