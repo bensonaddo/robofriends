3f86ad0f5165c8f255ce21952f7462eb6fd69461
baddo1 <benson.addo@gmfinancial.com>
2022-04-03 16:52:54 -0500

    loading api objects with condition

--- src/app.js
+++ src/app.js
@@ -18,8 +18,7 @@ class App extends Component{
     componentDidMount(){
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => {})
-            // .then(users => this.setState({ robots: users}))
+            .then(users => this.setState({ robots: users}))
     }
     onSearchChange = (event) => {
         // console.log(event.target.value);
